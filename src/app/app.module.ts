dc264324341c8ec1c323c680a8664c7945e63593
joyet-simon <joyet.simon@hotmail.fr>
2018-12-19 10:14:28 +0100

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MeteoPollutionComponent } from './meteo-pollution/meteo-pollution.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeteoPollutionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/meteo-pollution/meteo-pollution.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mp-meteo-pollution',
+  templateUrl: './meteo-pollution.component.html',
+  styleUrls: ['./meteo-pollution.component.scss']
+})
+export class MeteoPollutionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
